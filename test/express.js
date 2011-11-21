12d8a0c18524bd8dbf333f2d1a6b41f53d97e83a
James Halliday <mail@substack.net>
2011-11-20 23:53:42 -0800

    check for codes

--- test/express.js
+++ test/express.js
@@ -8,38 +8,48 @@ var root = __dirname + '/express';
 var fs = require('fs');
 var files = {
   'a.txt' : {
+    code : 200,
     type : 'text/plain',
     body : 'A!!!\n',
   },
   'b.txt' : {
+    code : 200,
     type : 'text/plain',
     body : 'B!!!\n',
   },
   'c.js' : {
+    code : 200,
     type : 'application/javascript',
     body : 'console.log(\'C!!!\');\n',
   },
   'd.js' : {
+    code : 200,
     type : 'application/javascript',
     body : 'console.log(\'C!!!\');\n',
   },
   'subdir/e.html' : {
+    code : 200,
     type : 'text/html',
     body : '<b>e!!</b>\n',
   },
   'subdir/index.html' : {
+    code : 200,
     type : 'text/html',
     body : 'index!!!\n',
   },
   'subdir' : {
+    code : 200,
     type : 'text/html',
     body : 'index!!!\n',
   },
+  '404' : {
+    code : 404
+  }
 };
 
 test('express', function (t) {
   var filenames = Object.keys(files);
-  t.plan(filenames.length * 2);
+  t.plan(filenames.length * 3 - 2);
   var port = Math.floor(Math.random() * ((1<<16) - 1e4) + 1e4);
   
   var app = express.createServer();
@@ -50,11 +60,20 @@ test('express', function (t) {
       var uri = 'http://localhost:' + port + '/' + file;
       request.get(uri, function (err, res, body) {
         if (err) t.fail(err);
-        t.equal(
-          res.headers['content-type'], files[file].type,
-          'content-type for ' + file
-        );
-        t.equal(body, files[file].body, 'body for ' + file);
+        var r = files[file];
+        
+        t.equal(r.code, res.statusCode, 'code for ' + file);
+        
+        if (r.type !== undefined) {
+          t.equal(
+            res.headers['content-type'], r.type,
+            'content-type for ' + file
+          );
+        }
+        
+        if (r.body !== undefined) {
+          t.equal(body, r.body, 'body for ' + file);
+        }
         
         if (--pending === 0) {
           app.close();
