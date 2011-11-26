22a84837c353187f0202ac530bcd4b9e2f0413c5
James Halliday <mail@substack.net>
2011-11-25 19:44:08 -0800

    use showdir in the express example how about

--- example/express.js
+++ example/express.js
@@ -2,7 +2,7 @@ var express = require('express');
 var ecstatic = require('../lib/ecstatic');
 
 var app = express.createServer();
-app.use(ecstatic(__dirname + '/public', { showDir : true }));
+app.use(ecstatic(__dirname + '/public', { showdir : true }));
 app.listen(8080);
 
 console.log('Listening on :8080');
