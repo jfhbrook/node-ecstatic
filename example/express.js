7a5cac36db7abd8a95b4b973ebe48bcca8f3b631
Christian Howe <coderarity@gmail.com>
2012-02-27 19:13:13 -0500

    Fixed 404.html problems.

--- example/express.js
+++ example/express.js
@@ -2,7 +2,7 @@ var express = require('express');
 var ecstatic = require('../lib/ecstatic');
 
 var app = express.createServer();
-app.use(ecstatic(__dirname + '/public', { showdir : true }));
+app.use(ecstatic({ root : __dirname + '/public', showdir : false }));
 app.listen(8080);
 
 console.log('Listening on :8080');
