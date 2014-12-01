3e3ce420ef5526ee207179d7fa1b186cdb6b79cc
James Halliday <mail@substack.net>
2014-11-30 17:19:28 -0800

    range past the end test

--- test/range.js
+++ test/range.js
@@ -4,7 +4,7 @@ var test = require('tap').test,
     request = require('request');
 
 test('range', function (t) {
-  t.plan(3);
+  t.plan(4);
   var server = http.createServer(ecstatic(__dirname + '/public/subdir'));
   t.on('end', function () { server.close() })
  
@@ -18,6 +18,27 @@ test('range', function (t) {
       t.ifError(err);
       t.equal(res.statusCode, 206, 'partial content status code');
       t.equal(body, 'e!!');
+      t.equal(parseInt(res.headers['content-length']), body.length);
+    });
+  });
+});
+
+test('range past the end', function (t) {
+  t.plan(4);
+  var server = http.createServer(ecstatic(__dirname + '/public/subdir'));
+  t.on('end', function () { server.close() })
+ 
+  server.listen(0, function () {
+    var port = server.address().port;
+    var opts = {
+      uri: 'http://localhost:' + port + '/e.html',
+      headers: { range: '3-500' }
+    };
+    request.get(opts, function (err, res, body) {
+      t.ifError(err);
+      t.equal(res.statusCode, 206, 'partial content status code');
+      t.equal(body, 'e!!</b>\n');
+      t.equal(parseInt(res.headers['content-length']), body.length);
     });
   });
 });
