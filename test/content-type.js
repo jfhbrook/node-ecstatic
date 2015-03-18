76f13518cb72fa411c506653fd0e756fa8b9cf38
Joshua Holbrook <josh.holbrook@gmail.com>
2015-03-18 19:42:36 -0400

    Fix content-type default test

--- test/content-type.js
+++ test/content-type.js
@@ -1,27 +1,26 @@
 var test = require('tap').test,
     ecstatic = require('../'),
-    http = require('http');
+    http = require('http'),
+    request = require('request');
 
-function do_test (t, server_options, result) {
-  var server = http.createServer(ecstatic(__dirname + '/public/',
-    server_options));
-  t.on('end', function () { server.close() })
-  server.listen(0, function () {
-    http.get({
-      host: 'localhost',
-      port: server.address().port,
-      path: '/f_f'
-    }).on('response', function (res) {
-      t.equal(res.headers['content-type'], result);
+test('default default contentType', function(t) {
+  var server = http.createServer(ecstatic({
+    root: __dirname + '/public/',
+    contentType: 'text/plain'
+  }));
+
+  t.plan(3);
+
+  t.on('end', function() { server.close(); });
+
+  server.listen(0, function() {
+    var port = server.address().port;
+    request.get('http://localhost:' + port + '/f_f', function(err, res, body) {
+      t.ifError(err);
+      t.equal(res.statusCode, 200);
+      t.equal(res.headers['content-type'], 'text/plain; charset=UTF-8');
       t.end();
     });
   });
-}
-
-test('contentType Header return specified', function (t) {
-  do_test(t, {contentType: 'text/html'}, 'text/html; charset=UTF-8');
 });
 
-test('contentType Header return default', function (t) {
-  do_test(t, {}, 'application/octet-stream; charset=utf-8');
-});
