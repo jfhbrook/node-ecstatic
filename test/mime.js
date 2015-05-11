e22dc921ff2a90f30c7c923122c09584df311049
dotnetcarpenter <jon.ronnenberg@gmail.com>
2015-05-11 16:26:31 +0200

    test mime package

---
+++ test/mime.js
@@ -0,0 +1,47 @@
+var test = require('tap').test,
+	  mime;
+    
+function setup() {
+  mime = require('mime');
+}
+function teardown(t) {
+  t && t.end();
+  mime = null;
+}
+
+test('mime package lookup', function(t) {
+  setup();
+  
+  t.plan(4);
+  
+  t.equal(mime.lookup('/path/to/file.txt'), 'text/plain');
+  t.equal(mime.lookup('file.txt'), 'text/plain');
+  t.equal(mime.lookup('.TXT'), 'text/plain');
+  t.equal(mime.lookup('htm'), 'text/html');
+  
+  teardown(t);
+});
+
+test('custom definition of mime-type with the mime package', function(t) {
+  setup();
+  
+  t.plan(1);
+  
+  mime.define({
+    'application/xml': ['opml']
+  });
+  t.equal(mime.lookup('.opml'), 'application/xml');
+  
+  teardown(t);
+});
+
+test('custom definition of mime-type with a .types file', function(t) {
+  setup();
+  
+  t.plan(1);
+  
+  mime.load('public/custom_mime_type.types');
+  t.equal(mime.lookup('.opml'), 'application/xml');
+  
+  teardown(t);
+});
