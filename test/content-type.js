140872fecd3a9c339766221214141fcc7872d7fc
dotnetcarpenter <jon.ronnenberg@gmail.com>
2015-05-11 18:13:32 +0200

    tried to test what happens if a wrong path is given to .types file but did not work

--- test/content-type.js
+++ test/content-type.js
@@ -1,14 +1,23 @@
 /// <reference path="../typings/node/node.d.ts"/>
 var test = require('tap').test,
-    ecstatic = require('../'),
     http = require('http'),
-    request = require('request');
+    request = require('request'),
+    ecstatic;
+
+function setup(opts) {
+  ecstatic = require('../');
+  return http.createServer(ecstatic(opts));
+}
+function teardown(t) {
+  t && t.end();
+  ecstatic = null;
+}
 
 test('default default contentType', function(t) {
-  var server = http.createServer(ecstatic({
+  var server = setup({
     root: __dirname + '/public/',
     contentType: 'text/plain'
-  }));
+  });
 
   t.plan(3);
 
@@ -20,18 +29,18 @@ test('default default contentType', function(t) {
       t.ifError(err);
       t.equal(res.statusCode, 200);
       t.equal(res.headers['content-type'], 'text/plain; charset=UTF-8');
-      t.end();
+      teardown(t);
     });
   });
 });
 
 test('custom contentType', function(t) {
-  var server = http.createServer(ecstatic({
+  var server = setup({
     root: __dirname + '/public/',
     mimetype: {
       'application/xml': ['opml']
     }
-  }));
+  });
 
   t.plan(3);
 
@@ -43,16 +52,16 @@ test('custom contentType', function(t) {
       t.ifError(err);
       t.equal(res.statusCode, 200);
       t.equal(res.headers['content-type'], 'application/xml; charset=utf-8');
-      t.end();
+      teardown();
     });
   });
 });
 
-test('custom contentType', function(t) {
-  var server = http.createServer(ecstatic({
+test('custom contentType via .types file', function(t) {
+  var server = setup({
     root: __dirname + '/public/',
     'mime-types': 'custom_mime_type.types'
-  }));
+  });
 
   t.plan(3);
 
@@ -64,7 +73,30 @@ test('custom contentType', function(t) {
       t.ifError(err);
       t.equal(res.statusCode, 200);
       t.equal(res.headers['content-type'], 'application/xml; charset=utf-8');
-      t.end();
+      teardown();
     });
   });
 });
+
+/* NOT SURE HOW TO TEST THIS - SEE COMMENT IN PR
+test('warning when custom contentType .types file does not exist', function(t) {
+  var server = setup({
+    root: __dirname + '/public/',
+    'mime-types': 'this_file_does_not_exist.types'
+  });
+
+  t.plan(3);
+
+  t.on('end', function() { server.close(); });
+
+  server.listen(0, function() {
+    var port = server.address().port;
+    request.get('http://localhost:' + port + '/custom_mime_type.opml', function(err, res, body) {
+      t.ifError(err);
+      t.equal(res.statusCode, 200);
+      t.equal(res.headers['content-type'], 'application/xml; charset=utf-8');
+      teardown();
+    });
+  });
+});
+*/
