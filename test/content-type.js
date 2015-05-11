04eaa9fc621635580d4feccb9c4e2288a00f602d
dotnetcarpenter <jon.ronnenberg@gmail.com>
2015-05-11 14:40:39 +0200

    added .opml file as text fixture

--- test/content-type.js
+++ test/content-type.js
@@ -24,3 +24,22 @@ test('default default contentType', function(t) {
   });
 });
 
+test('custom contentType', function(t) {
+  var server = http.createServer(ecstatic({
+    root: __dirname + '/public/'
+  }));
+
+  t.plan(3);
+
+  t.on('end', function() { server.close(); });
+
+  server.listen(0, function() {
+    var port = server.address().port;
+    request.get('http://localhost:' + port + '/custom_mime_type.opml', function(err, res, body) {
+      t.ifError(err);
+      t.equal(res.statusCode, 200);
+      t.equal(res.headers['content-type'], 'application/xml; charset=UTF-8');
+      t.end();
+    });
+  });
+});
