a68034581a9371ccc86c65070083d3ec4d506c0d
Jon Ege Ronnenberg <jon.ronnenberg@gmail.com>
2015-05-12 22:20:40 +0000

    pass the malformed tests on linux

--- test/malformed-dir.js
+++ test/malformed-dir.js
@@ -1,21 +1,20 @@
 var test = require('tap').test,
     ecstatic = require('../lib/ecstatic'),
-    http = require('http');
+    http = require('http'),
+    request = require('request');
 
 test('malformed showdir uri', function (t) {
   var server = http.createServer(ecstatic(__dirname, { showDir: true }));
   
-  t.plan(1);
+  t.plan(2);
   
   server.listen(0, function () {
-    var r = http.get({
-      host: 'localhost',
-      port: server.address().port,
-      path: '/?%'
-    });
-    r.on('response', function (res) {
+    
+    request.get('http://localhost:' + server.address().port + '/?%', function (err, res, body) {
+      t.ifError(err);
       t.equal(res.statusCode, 400);
       server.close(function() { t.end(); });
     });
+
   });
 });
--- test/malformed.js
+++ test/malformed.js
@@ -1,19 +1,16 @@
 var test = require('tap').test,
     ecstatic = require('../lib/ecstatic'),
-    http = require('http');
+    http = require('http'),
+    request = require('request');
 
 test('malformed uri', function (t) {
   var server = http.createServer(ecstatic(__dirname));
   
-  t.plan(1);
+  t.plan(2);
   
   server.listen(0, function () {
-    var r = http.get({
-      host: 'localhost',
-      port: server.address().port,
-      path: '/%'
-    });
-    r.on('response', function (res) {
+    request.get('http://localhost:' + server.address().port + '/%', function (err, res, body) {
+      t.ifError(err);
       t.equal(res.statusCode, 400);
       server.close(function() { t.end(); });
     });
