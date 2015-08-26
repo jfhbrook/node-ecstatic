790fe5f72c48cb217e14962caf9f8c2a3e12087a
Jacob Burden <Jacob.JW.Burden@gmail.com>
2015-08-26 19:25:08 -0400

    refactored test to utilize the env options of spawn when setting process.env.PORT

--- test/process-env-port.js
+++ test/process-env-port.js
@@ -1,23 +1,30 @@
 var test = require('tap').test,
     request = require('request'),
     spawn = require('child_process').spawn,
-    insanePorts = [ -Infinity, 1023, 65537, Infinity, 'wow', null, undefined]
+    sanePort = getRandomInt(1025, 65536),
+    insanePorts = [-Infinity, 1023, 65537, Infinity, 'wow', null, undefined]
 
 test('sane port', function (t) {
   t.plan(2)
-  process.env.PORT = getRandomInt(1025, 65536)
-  var ecstatic = spawn(process.execPath, [__dirname + '/../lib/ecstatic.js'])
+  var ecstatic = spawn(process.execPath, [__dirname + '/../lib/ecstatic.js'], {
+    env: {
+      PORT: sanePort
+    }
+  })
   ecstatic.stdout.on('data', function (data) {
     t.pass('ecstatic should be started')
-    checkServerIsRunning('http://0.0.0.0:' + process.env.PORT, ecstatic, t)
+    checkServerIsRunning('http://0.0.0.0:' + sanePort, ecstatic, t)
   })
 })
 
 insanePorts.forEach(function (port) {
   test('insane port: ' + port, function (t) {
     t.plan(2)
-    process.env.PORT = port
-    var ecstatic = spawn(process.execPath, [__dirname + '/../lib/ecstatic.js'])
+    var ecstatic = spawn(process.execPath, [__dirname + '/../lib/ecstatic.js'], {
+      env: {
+        PORT: port
+      }
+    })
     ecstatic.stdout.on('data', function (data) {
       t.pass('ecstatic should be started')
       checkServerIsRunning('http://0.0.0.0:8000', ecstatic, t)
