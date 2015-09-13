8103a7bcfc8412d40ee7418d1b3ea649058e3d77
Joshua Holbrook <josh.holbrook@gmail.com>
2015-09-12 22:09:45 -0400

    Tweaks and fixes to process env port tests

--- test/process-env-port.js
+++ test/process-env-port.js
@@ -3,46 +3,52 @@ var test = require('tap').test,
     spawn = require('child_process').spawn,
     sanePort = getRandomInt(1025, 65536),
     floatingPointPort = 9090.86,
-    insanePorts = [-Infinity, 1023, 65537, Infinity, 'wow', null, undefined]
+    insanePorts = [1023, 65537, Infinity, 'wow'];
 
 test('sane port', function (t) {
-  startServer('http://0.0.0.0:' + sanePort, sanePort, t)
-})
+  startServer('http://0.0.0.0:' + sanePort, sanePort, t);
+});
 
 test('floating point port', function (t) {
-  startServer('http://0.0.0.0:9090', floatingPointPort, t)
-})
+  startServer('http://0.0.0.0:9090', floatingPointPort, t);
+});
 
 insanePorts.forEach(function (port) {
   test('insane port: ' + port, function (t) {
-    startServer('http://0.0.0.0:8000', port, t)
+    startServer('http://0.0.0.0:8000', port, t);
   })
-})
+});
 
 function getRandomInt (min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function checkServerIsRunning (url, ps, t) {
   request(url, function (err, res, body) {
-    if (!err && res.statusCode === 200) {
-      t.pass('a successful request from the server was made')
-      ps.kill('SIGINT')
+    if (!err && res.statusCode !== 500) {
+      t.pass('a successful request from the server was made');
     } else {
-      t.fail('the server could not be reached')
+      t.fail('the server could not be reached');
     }
+    ps.kill('SIGTERM');
   })
 }
 
 function startServer (url, port, t) {
-  t.plan(2)
+  t.plan(2);
   var ecstatic = spawn(process.execPath, [__dirname + '/../lib/ecstatic.js'], {
     env: {
-      PORT: port
+      PORT: String(port)
     }
-  })
+  });
+
+  /*
+  ecstatic.stderr.pipe(process.stderr);
+  ecstatic.stdout.pipe(process.stderr);
+  */
+
   ecstatic.stdout.on('data', function (data) {
-    t.pass('ecstatic should be started')
-    checkServerIsRunning(url, ecstatic, t)
-  })
+    t.pass('ecstatic should be started');
+    checkServerIsRunning(url, ecstatic, t);
+  });
 }
