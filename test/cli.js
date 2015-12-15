ba4bb1e233d61e36f217f487e8805b32cc26aad2
dotnetcarpenter <jon.ronnenberg@gmail.com>
2015-12-15 14:13:25 +0100

    [cli] port test

--- test/cli.js
+++ test/cli.js
@@ -5,40 +5,95 @@
 var test = require('tap').test,
     request = require('request'),
     spawn = require('child_process').spawn,
-    node = process.execPath
+    node = process.execPath,
+    defaultUrl = 'http://localhost',
+    defaultPort = 8000,
+    getRandomPort = (function() {
+      var usedPorts = [];
+      return function() {
+        var port = getRandomInt(1025, 65536);
+        if(usedPorts.indexOf(port) > -1)
+          return getRandomPort();
+  
+        usedPorts.push(port);
+        return port;
+      }
+    }());
 
-function startEcstatic(options) {
-  options = options || [];
-  return spawn(node, [require.resolve('../lib/ecstatic.js')].concat(options));
-}
 
 test('setting port via cli - default port', function(t) {
-  t.plan(1);
-  var ecstatic = startEcstatic();
-  
+  var port = defaultPort;
+  var options = ['.'];
+  var ecstatic = startEcstatic(options)
+  var portRegex = /(\d{4})/;
+
+ecstatic.stderr.setEncoding('utf8');
+ecstatic.stderr.on("data", console.log.bind(console, "error:"))
+
   ecstatic.stdout.on("data", function(data) {
-    var portRegex = /(\d{4})/;
-    var expected = 8000,
+    var expected = port,
         actual = portRegex.test(data) && portRegex.exec(data)[0]|0;
     t.equal(actual, expected, 'The default port number should be 8000');
     ecstatic.kill('SIGTERM');
-  });  
+    t.end();
+  });
+  
   /*ecstatic.stderr.on("data", console.log.bind(console, "error:"))
   ecstatic.stdin.on("data", console.log.bind(console, "in:"))*/
 });
+function startEcstatic(args, options) {
+  console.log("args", args);
+  return spawn(node, [require.resolve('../lib/ecstatic.js')].concat(args));
+}
 
 test('setting port via cli - custom port', function(t) {
-  t.plan(1);
-  var ecstatic = startEcstatic(['--port ' + 1213]);
+  var port = getRandomPort();
+  var options = ['.', '--port', port];
+  var ecstatic = startEcstatic(options);
+
+ecstatic.stderr.setEncoding('utf8');
+ecstatic.stderr.on("data", console.log.bind(console, "error:"))
+
   
   ecstatic.stdout.on("data", function(data) {
     var portRegex = /(\d{4})/;
-    var expected = 1213,
+    var expected = port,
         actual = portRegex.test(data) && portRegex.exec(data)[0]|0;
-    t.equal(actual, expected, 'The port number should be 1213');
+    t.equal(actual, expected, 'The port number should be ' + port);
     ecstatic.kill('SIGTERM');
   });
 });
+
+function startEcstatic_OLD(options) {
+  t.plan(1 + options.tests.length);
+  var tests = options.tests;
+  var ecstatic = spawn(node, [require.resolve('../lib/ecstatic.js')].concat(options));
+  
+  checkServerIsRunning(url, t);
+  ecstatic.stdout.on('data', function(data) {
+    tests.forEach(function(test) {
+      test(data, t);
+    });
+    ecstatic.kill('SIGTERM');
+  });
+}
+
+function getRandomInt (min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function checkServerIsRunning (url, ps, t) {
+  request(url, function (err, res, body) {
+    if (!err && res.statusCode !== 500) {
+      t.pass('a successful request from the server was made');
+    } else {
+      t.fail('the server could not be reached');
+    }
+  })
+}
+
+
+/*
 
 test('setting mimeTypes via cli', function(t) {
   t.plan(1);
@@ -51,7 +106,7 @@ test('setting mimeTypes via cli', function(t) {
     t.ok(true, 'test something', true);
     ecstatic.kill('SIGTERM');
   });
-  /*ecstatic.stderr.setEncoding('utf8');
+  ecstatic.stderr.setEncoding('utf8');
   ecstatic.stderr.on("data", console.log.bind(console, "error:"))
-  ecstatic.stdin.on("data", console.log.bind(console, "in:"))*/
-});
+  ecstatic.stdin.on("data", console.log.bind(console, "in:"))
+});*/
