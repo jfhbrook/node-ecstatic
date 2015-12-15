cb84d715b25ac42eddbc3ca1b13cd651f14341ad
dotnetcarpenter <jon.ronnenberg@gmail.com>
2015-12-15 15:22:58 +0100

    tap t.teardown is undefined

--- test/cli.js
+++ test/cli.js
@@ -23,10 +23,19 @@ var test = require('tap').test,
 
 
 test('setting port via cli - default port', function(t) {
+  t.plan(2);
+
   var port = defaultPort;
   var options = ['.'];
   var ecstatic = startEcstatic(options)
+  
+  console.log(t.teardown);
+  
+  t.teardown(function() {
+    ecstatic.kill('SIGTERM');    
+  });
 
+//TODO: remove error handler
 ecstatic.stderr.setEncoding('utf8');
 ecstatic.stderr.on("data", console.log.bind(console, "error:"))
 
@@ -34,12 +43,9 @@ ecstatic.stderr.on("data", console.log.bind(console, "error:"))
     var expected = port,
         actual = portRegex.test(data) && portRegex.exec(data)[0]|0;
     t.equal(actual, expected, 'The default port number should be 8000');
-    ecstatic.kill('SIGTERM');
-    t.end();
   });
   
-  /*ecstatic.stderr.on("data", console.log.bind(console, "error:"))
-  ecstatic.stdin.on("data", console.log.bind(console, "in:"))*/
+  checkServerIsRunning(defaultUrl + ':' + port, t);
 });
 function startEcstatic(args, options) {
   console.log("args", args);
@@ -47,41 +53,34 @@ function startEcstatic(args, options) {
 }
 
 test('setting port via cli - custom port', function(t) {
+  t.plan(2);
+
   var port = getRandomPort();
   var options = ['.', '--port', port];
   var ecstatic = startEcstatic(options);
 
+//TODO: remove error handler
 ecstatic.stderr.setEncoding('utf8');
 ecstatic.stderr.on("data", console.log.bind(console, "error:"))
-
   
-  ecstatic.stdout.on("data", function(data) {
+  t.teardown(function() {
+    ecstatic.kill('SIGTERM');    
+  });
+  
+  ecstatic.stdout.on('data', function(data) {
     var expected = port,
         actual = portRegex.test(data) && portRegex.exec(data)[0]|0;
     t.equal(actual, expected, 'The port number should be ' + port);
-    ecstatic.kill('SIGTERM');
   });
-});
-
-function startEcstatic_OLD(options) {
-  t.plan(1 + options.tests.length);
-  var tests = options.tests;
-  var ecstatic = spawn(node, [require.resolve('../lib/ecstatic.js')].concat(options));
   
-  checkServerIsRunning(url, t);
-  ecstatic.stdout.on('data', function(data) {
-    tests.forEach(function(test) {
-      test(data, t);
-    });
-    ecstatic.kill('SIGTERM');
-  });
-}
+  checkServerIsRunning(defaultUrl + ':' + port, t);
+});
 
 function getRandomInt (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function checkServerIsRunning (url, ps, t) {
+function checkServerIsRunning (url, t) {
   request(url, function (err, res, body) {
     if (!err && res.statusCode !== 500) {
       t.pass('a successful request from the server was made');
