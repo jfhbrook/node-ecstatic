eb6f51de746cfd365582f6f690b715b9db19cc7c
dotnetcarpenter <jon.ronnenberg@gmail.com>
2015-12-18 12:47:11 +0100

    test setting mimetype via directly via cli

--- test/cli.js
+++ test/cli.js
@@ -1,13 +1,14 @@
 "use strict";
 
-/* this test suit is incomplete  2015-12-14 */
+/* this test suit is incomplete  2015-12-17 */
 
 var test = require('tap').test,
     request = require('request'),
     spawn = require('child_process').spawn,
+    path = require('path'),
     node = process.execPath,
     defaultUrl = 'http://localhost',
-    portRegex = /(\d{4,5})/,
+    portRegex = /(\d{4,5})/, //NOTE: might delete
     defaultPort = 8000,
     getRandomPort = (function() {
       var usedPorts = [];
@@ -31,6 +32,7 @@ test('setting port via cli - default port', function(t) {
   
   tearDown(ecstatic, t);
 
+  //NOTE: might delete
   ecstatic.stdout.on("data", function(data) {
     var expected = port,
         actual = portRegex.test(data) && portRegex.exec(data)[0]|0;
@@ -48,7 +50,7 @@ test('setting port via cli - custom port', function(t) {
   var ecstatic = startEcstatic(options);
 
   tearDown(ecstatic, t);
-  
+  //NOTE: might delete
   ecstatic.stdout.on('data', function(data) {
     var expected = port,
         actual = portRegex.test(data) && portRegex.exec(data)[0]|0;
@@ -58,25 +60,46 @@ test('setting port via cli - custom port', function(t) {
   checkServerIsRunning(defaultUrl + ':' + port, t);
 });
 
-test('setting mimeTypes via cli', function(t) {
+test('setting mimeTypes via cli - .types file', function(t) {
   t.plan(2);
   
   var port = getRandomPort();
-  var options = ['.', '--port', port];
+  var root = path.resolve(__dirname, 'public/');
+  var pathMimetypeFile = path.resolve(__dirname, 'fixtures/custom_mime_type.types');
+  var options = [root, '--port', port, '--mimetypes', pathMimetypeFile];
   var ecstatic = startEcstatic(options);
   
   tearDown(ecstatic, t);
   
-  ecstatic.stdout.on("data", function(data) {
-    t.ok(true, 'test something');
+  ecstatic.stdout.on('data', function(data) {
+    t.pass('ecstatic should be started');
+    checkServerIsRunning(defaultUrl + ':' + port + '/custom_mime_type.opml', t);
   });
+
+});
+
+test('setting mimeTypes via cli - ', function(t) {
+  t.plan(3);
   
+  var port = getRandomPort();
+  var root = path.resolve(__dirname, 'public/');
+  var mimeType = ['--mimeTypes', '{ "application/x-my-type": ["opml"] }'];
+  var options = [root, '--port', port, '--mimetypes'].concat(mimeType);
+  var ecstatic = startEcstatic(options);
   //TODO: remove error handler
-  ecstatic.stderr.setEncoding('utf8');
-  ecstatic.stderr.on("data", console.log.bind(console, "error:"))
-  ecstatic.stdin.on("data", console.log.bind(console, "in:"))
+  ecstatic.stderr.pipe(process.stderr);
+  ecstatic.stdout.pipe(process.stderr);
   
-  checkServerIsRunning(defaultUrl + ':' + port, t);
+  tearDown(ecstatic, t);
+  
+  ecstatic.stdout.on('data', function(data) {
+    t.pass('ecstatic should be started');
+    checkServerIsRunning(defaultUrl + ':' + port + '/custom_mime_type.opml', t)
+      .then(function(res) {
+        t.equal(res.headers['content-type'], 'application/x-my-type; charset=utf-8');
+      });
+  });
+
 });
 
 function getRandomInt (min, max) {
@@ -84,23 +107,28 @@ function getRandomInt (min, max) {
 }
 
 function startEcstatic(args, options) {
-  console.log("args", args);
+  // console.log("args", args);
   return spawn(node, [require.resolve('../lib/ecstatic.js')].concat(args));
 }
 
 function checkServerIsRunning (url, t) {
+  var cb;
+  var p = { then: function(f) { cb = f; }, resolve: function(v) { (cb && cb(v)); }};
   request(url, function (err, res, body) {
     if (!err && res.statusCode !== 500) {
       t.pass('a successful request from the server was made');
+      p.resolve(res);
     } else {
-      t.fail('the server could not be reached');
+      t.fail('the server could not be reached @ ' + url);
+      p.resolve();
     }
   })
+  return p;
 }
 
 function tearDown(ps, t) {
   t.tearDown(function() {
-    console.log("KILL!")
-    ps.kill('SIGTERM');    
+    // console.log("KILL!")
+    ps.kill('SIGTERM');
   });
 }
